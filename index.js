93b510a3611e287788f765f74e0e9820446f9d2f
Sohan Ahmed <82075290+asohaahmed@users.noreply.github.com>
2021-07-02 23:32:47 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var frist = prompt("enter your frist number :");
+var last = prompt("enter your last number");
+ var frist  =parseInt(frist,10);
+  var last = parseInt(last,10);
+
+ var sum =frist+last;
+var sub =frist-last;
+var vag =frist/last;
+var puron = frist*last;
+
+
+
+document.write( "your minus number = " + sum);
+document.write("<br/>" + "your minus number = " + sub);
+document.write("<br/>" + "your vag number = " + vag);
+document.write("<br/>" + "your puron number = " + puron);
+
+
+
+
